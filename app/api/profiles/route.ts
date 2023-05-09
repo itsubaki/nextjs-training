ee0fe2cbae5281bb54e08a9589df7d359fbe0944
itsubaki <1759459+itsubaki@users.noreply.github.com>
2023-05-09 06:41:23 +0000

    Add dynamic config

--- app/api/profiles/route.ts
+++ app/api/profiles/route.ts
@@ -1,24 +1,28 @@
 import kv from "@vercel/kv";
 import { NextResponse } from "next/server";
 
+// https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#dynamic
+export const dynamic = "force-dynamic";
+
 // https://nextjs.org/docs/app/building-your-application/routing/router-handlers
 export async function GET() {
-  const profile = {
-    name: "itsubaki",
-    github: "https://github.com/itsubaki",
-    timestamp: Date.now(),
-  };
-  try {
-    await kv.set("profile", profile, { ex: 60, nx: true });
-  } catch (error) {
-    return NextResponse.json(error);
-  }
-
   try {
     const value = await kv.get("profile");
     if (value) {
       return NextResponse.json(value);
     }
+  } catch (error) {
+    return NextResponse.json(error);
+  }
+
+  const profile = {
+    name: "itsubaki",
+    github: "https://github.com/itsubaki",
+    timestamp: Date.now(),
+  };
+
+  try {
+    await kv.set("profile", profile, { ex: 60 });
   } catch (error) {
     return NextResponse.json(error);
   }
